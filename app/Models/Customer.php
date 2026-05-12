59c7ebdf0a33c1e5bdc98fc7893ff609a39f2510
Cauê Braga <cauebraga.dev@gmail.com>
2026-05-12 10:24:27 -0300

    refactor: update customer model structure

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,11 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Enums\CustomerStatus;
 class Customer extends Model
 {
     /** @use HasFactory<\Database\Factories\CustomerFactory> */
     use HasFactory;
+
+    protected $casts = [
+        'status' => CustomerStatus::class,
+    ];
 
     protected $fillable = [
         'name',
@@ -20,5 +24,15 @@ class Customer extends Model
         'city',
         'state',
         'zip_code',
+        'status'
     ];
+
+    public function toAuditArray(): array
+    {
+        return [
+            "name" => $this->name,
+            "email" => $this->email,
+            "type" => $this->type
+        ];
+    }
 }
